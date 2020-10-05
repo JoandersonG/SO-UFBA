740ccbe0d10a2bf5ddfa5903392e7d06f20c6354
Joanderson <joanderson.rikudo@yahoo.com>
2020-10-05 15:01:53 -0300

    multiplicação de matrizes funcionando

--- Prática 3/multmatriz.c
+++ Prática 3/multmatriz.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <pthread.h>
-#include <errno.h>
 #include <string.h>
 
 int** tasks;
@@ -25,44 +23,45 @@ double calculaValor(double* linha, double* coluna) {
     return total;
 }
 
-int getTaskLinhaA() {
-    //bloquear acesso por mais de uma thread. Isso funciona?
-    pthread_mutex_lock(&mutex);
-    int tn = tasks[taskNumber-1][0];
-    pthread_mutex_unlock(&mutex);
-    return tn;
-}
-int getTaskColunaB() {
-    //bloquear acesso por mais de uma thread. Isso funciona?
-    pthread_mutex_lock(&mutex);
-    int tn = tasks[taskNumber-1][1];
-    pthread_mutex_unlock(&mutex);
-    return tn;
-}
 void * calculaEmThread(void * args) {
 
 
-    //recupero argumentos
-    int threadAtual = *((int *) args); //não funciona apropriadamente
+    //recupera argumentos
+    int threadAtual = *((int *) args);
 
     //executa
-    while (taskNumber > 0) {
-        printf("thread: %d. Executando a task %d\n",threadAtual, taskNumber-1);
+    while (1) {
 
-        //recupera tarefa
-        int linhaA = getTaskLinhaA();
-        int colunaB = getTaskColunaB();
-        matrizProduto[linhaA][colunaB] = calculaValor(matrizA[linhaA],matrizB[colunaB]);
-        printf("calculado a task %d: multiplicando linha %d por coluna %d e total: %f\n", taskNumber - 1, linhaA, colunaB, matrizProduto[linhaA][colunaB]);
+        //bloqueia essa área do código para múltiplas threads não realizarem a mesma operação
+        pthread_mutex_lock(&mutex);
+        if (taskNumber <= 0) {
+            pthread_mutex_unlock(&mutex);
+            break;
+        }
         taskNumber--;
-    }
 
-    //salva valor
+        //recupera linha e coluna a serem multiplicadas
+        int linhaA = tasks[taskNumber][0];
+        int colunaB = tasks[taskNumber][1];
+
+        //fijm do bloqueio da érea de código
+        pthread_mutex_unlock(&mutex);
+
+        printf("thread: %d. Executando a task %d\n",threadAtual, taskNumber);
+
+        //executa operação e armazena na matriz final
+        matrizProduto[linhaA][colunaB] = calculaValor(matrizA[linhaA],matrizB[colunaB]);
+        printf("calculado a task %d: multiplicando linha %d por coluna %d e total: %f\n", taskNumber, linhaA, colunaB, matrizProduto[linhaA][colunaB]);
+
+    }
 }
 
 int main(int argc, char ** argv) {
 
     //testar se os argumentos são válidos (quantidade correta e ints válidos e se dá para fazer multiplicação [tam linha x colunas])
+    if(argc) {
+        //aqui os testes e mensagens de erro
+    }
 
     //armazena os argumentos em variáveis identificáveis
     int linhasA = atoi(argv[1]); //armazena a quantidade de linhas da matriz A
@@ -73,14 +72,13 @@ int main(int argc, char ** argv) {
     //define o máximo dee threads a serem usadas
     int QTD_THREADS = atoi(argv[5]);
     pthread_t threads[QTD_THREADS];
-    printf("qtd_threads informada: %d\n", QTD_THREADS);
 
     //cria as matrizes A e B
     matrizA = malloc(sizeof(double *) * linhasA);
     for (int i = 0; i < linhasA; ++i) {
         matrizA[i] = malloc(sizeof(double) * colunasA);
     }
-    //invertendo as colunas e linhas para permitir calcular usando linhas.
+    //invertendo as colunas e linhas de B para permitir calcular usando o vetor linha
     matrizB = malloc(sizeof(double *) * colunasB);
     for (int i = 0; i < colunasB; ++i) {
         matrizB[i] = malloc(sizeof(double) * linhasB);
@@ -89,13 +87,12 @@ int main(int argc, char ** argv) {
     //salvo globalmente o total de linhas de A para cálculos posteriores
     qtdLinhasA = linhasA;
 
-
     //cria uma matriz das dimensões que uma matriz produto precisa para o tamanho de A e B
     matrizProduto = malloc( sizeof(double*) * linhasA);
     for (int i = 0; i < linhasA; ++i) {
         matrizProduto[i] = malloc( sizeof(double) * colunasB);
     }
-    //todo: teste se alocação falhar
+    //todo: teste se alocações falharem
 
     //zerar as matrizes. preciso?
     for (int i = 0; i < linhasA; ++i) {
@@ -112,9 +109,9 @@ int main(int argc, char ** argv) {
             matrizB[j][i] = 0;
         }
     }
-    //printf("aqui2");
-    //necessário para usar aleatório. Temporariamente excluído para testar corretude
-    //todo: srand(time(0));;
+
+    //necessário para usar aleatório
+    srand(time(0));;
 
     //popula matriz A
     for (int i = 0; i < linhasA; ++i) {
@@ -141,16 +138,15 @@ int main(int argc, char ** argv) {
      * [L1, C3]
      * [L1, C4]
      * ...
-     * Com L1 sendo a linha de A a ser multiplicada pela coluna C2 e armazenada na matriz resultante em [1][2]
+     * Com L1 sendo o índice da linha de A a ser multiplicada pela coluna com índice C2
+     * e armazenada na matriz resultante em [L1][C2]
      */
 
-    
+
     //calcula a matriz produto
-    int qtdThreadsUsadas = 0;
     int posicaoTask = 0;
     for (int i = 0; i < linhasA; ++i) {
         for (int j = 0; j < colunasB; ++j) {
-
             //salvo as tarefas que deverão ser executadas pelas threads
             tasks[posicaoTask][0] = i; //multiplicar a linha i de A
             tasks[posicaoTask][1] = j; //pela coluna j de B
@@ -163,7 +159,7 @@ int main(int argc, char ** argv) {
         printf("tarefa %d: multiplicar linha %d por coluna %d\n ",i, tasks[i][0], tasks[i][1]);
     }
 
-    //iniciar as threads
+    //inicia as threads
     int tid[QTD_THREADS];
     for (int i = 0; i < QTD_THREADS; i++) {
         tid[i] = i;
@@ -174,16 +170,14 @@ int main(int argc, char ** argv) {
         printf("iniciando a thread %d. Seu id é %lu \n", i, threads[i]);
     }
 
-    //esperar threads finalizarem
+    //espera threads finalizarem
     for (int i = 0; i < QTD_THREADS; ++i) {
         int errorTest = pthread_join(threads[i], NULL);
         //printf("error test: %d\n", errorTest);
         if (errorTest != 0) {
             printf("Um erro ocorreu ao finalizar thread %d. Erro: %s\n", i, strerror(errorTest));
         }
-
     }
-
 
     printf("Matriz A:\n");
     for (int i = 0; i < linhasA; ++i) {
